e79dab4d6e5ee08fe03e337162f23b90c3eeaa39
Ashiqur Rahman Biplop <biplops22@gmail.com>
2023-04-23 17:06:29 +0600

    added by login page

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import moment from "moment";
 import logo from "../../../assets/logo.png";
-import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import { Button, Container} from "react-bootstrap";
 import Marquee from "react-fast-marquee";
-import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -24,24 +23,7 @@ const Header = () => {
           just some text....
         </Marquee>
       </div>
-      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-        <Container>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mx-auto">
-              <Nav.Link href="#features"><Link to={`/`}>Home</Link></Nav.Link>
-              <Nav.Link href="#pricing">About</Nav.Link>
-              <Nav.Link href="#pricing">Career</Nav.Link>
-            </Nav>
-            <Nav>
-              <Nav.Link href="#deets">profile</Nav.Link>
-              <Nav.Link eventKey={2} href="#memes">
-                <Button variant="secondary">Login</Button>
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      
     </Container>
   );
 };
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,35 +1,55 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
 import Main from "../layout/Main";
 import Home from "../pages/Home/Home/Home";
 import Category from "../pages/Home/Category/Category";
 import NewsLayout from "../layout/NewsLayout";
 import News from "../pages/News/News/News";
+import LoginLayouts from "../LoginLayouts/LoginLayouts";
+import Login from "../pages/login/Login";
+import Register from "../pages/login/Register";
 
 const router = createBrowserRouter([
-    {
-        path:'/',
-        element: <Main></Main>,
-        children:[
-            {
-                path:'/',
-                element: <Home></Home>
-            },
-            {
-                path: '/category/:id',
-                element:<Category></Category>,
-                loader: ({params})=>fetch(`http://localhost:5000/categories/${params.id}`)
-            },
-        ]
-    },
-    {
-        path:'news',
-        element:<NewsLayout></NewsLayout>,
-        children:[
-            {
-                path:':id',
-                element:<News></News>
-            }
-        ]
-    }
+  {
+    path: "/",
+    element: <LoginLayouts></LoginLayouts>,
+    children: [
+      {
+        path: "/",
+        element:<Navigate to="/category/0"></Navigate>
+      },
+      {
+        path: "login",
+        element: <Login></Login>,
+      },
+      {
+        path: "register",
+        element: <Register></Register>,
+      },
+    ],
+  },
+  {
+    path: "category",
+    element: <Main></Main>,
+    children: [
+      {
+        path: ":id",
+        element: <Category></Category>,
+        loader: ({ params }) =>
+          fetch(`http://localhost:5000/categories/${params.id}`),
+      },
+    ],
+  },
+  {
+    path: "news",
+    element: <NewsLayout></NewsLayout>,
+    children: [
+      {
+        path: ":id",
+        element: <News></News>,
+        loader: ({ params }) =>
+          fetch(`http://localhost:5000/News/${params.id}`),
+      },
+    ],
+  },
 ]);
-export default router
+export default router;
